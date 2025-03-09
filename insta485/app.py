0aaa81fcecdde304581d1af59ead776ea4129f07
Haohong Shang <shanghaohong@sjtu.edu.cn>
2025-03-09 01:04:39 -0500

    complete index

--- insta485/app.py
+++ insta485/app.py
@@ -1,10 +1,28 @@
+import os
+import json
+import random
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+STATIC_FOLDER = os.path.join(os.path.dirname(__file__), 'static')
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    file_path = os.path.join(STATIC_FOLDER, 'data/data.json')
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    
+    idx = random.randint(0, len(data) - 1)
+    
+    context = {
+        'idx': idx,
+        's1': data[idx]["long_sentence"],
+        's2': data[idx]["incorrect_causation_summary"],
+    }
+    
+    return render_template('index.html', **context)
 
 @app.route('/submit', methods=['POST'])
 def submit():
--- insta485/views/__init__.py
+++ insta485/views/__init__.py
@@ -1,7 +1,2 @@
 """Views, one for each Insta485 page."""
-from insta485.views.index_old import *
-from insta485.views.users import *
-from insta485.views.explore import *
-from insta485.views.accounts import *
-from insta485.views.posts import *
-from insta485.views.rapi import *
+from insta485.views.index import *
